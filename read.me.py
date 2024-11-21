02cdf4c885269f75e21c3a9375529356f40be9db
MOTYA234 <apaz26043@gmail.com>
2024-11-21 15:29:55 +0300

    plohoe

--- read.me.py
+++ read.me.py
@@ -2,12 +2,11 @@ order = input("Заказано : ")
 if order == "печенье":
     print(input("начинка = "))
 else:
-    print("извините хихихи")
-    exit()
+    print("извините хихихи")    
 if order == "печенье с джемом":
     print("заказано популярное печенье:TRUE")
 elif order ==  "печенье с кремом":
     print("заказано популярное печеньe:TRUE")
 else:
     print("заказано популярное печенье:FALSE")
-
+print("thx for all")
